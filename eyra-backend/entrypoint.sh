84136d6d44a112535505967d5bcfa33282b0ca7b
anaGG07 <ana24garcia07@gmail.com>
2025-04-13 13:27:54 +0200

    Add entrypoint.sh

--- eyra-backend/entrypoint.sh
+++ eyra-backend/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "🔧 Instalando dependencias de Symfony..."
+echo "Instalando dependencias..."
 composer install --no-interaction --prefer-dist --optimize-autoloader
+
+echo "Ejecutando migraciones de base de datos..."
+php bin/console doctrine:schema:update --force
 
 echo "Arrancando PHP-FPM..."
 exec php-fpm
