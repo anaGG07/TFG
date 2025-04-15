2b455e16a7cc87fbe25dd938b8efa7956f92c24f
anaGG07 <ana24garcia07@gmail.com>
2025-04-15 11:54:58 +0200

    Created nginx.conf (backend)

--- eyra-backend/entrypoint.sh
+++ eyra-backend/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 echo "Instalando dependencias..."
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-echo "Ejecutando migraciones de base de datos..."
-php bin/console doctrine:schema:update --force
+echo "Ejecutando migraciones Doctrine..."
+php bin/console doctrine:migrations:migrate --no-interaction
 
 echo "Arrancando PHP-FPM..."
-php-fpm
+exec php-fpm
