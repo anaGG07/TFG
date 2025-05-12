6ed01394eeb9035af547ceff40b444da02775b9a
anaGG07 <ana24garcia07@gmail.com>
2025-05-12 12:37:45 +0200

    Modify LoginPage

--- eyra-frontend/src/pages/LoginPage.tsx
+++ eyra-frontend/src/pages/LoginPage.tsx
@@ -35,7 +35,7 @@ const LoginPage = () => {
     };
 
     checkExistingSession();
-  }, [user, checkAuth, navigate]);
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
