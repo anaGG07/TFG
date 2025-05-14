9b184a3363583fb345d903502443420dcdc7ae18
anaGG07 <ana24garcia07@gmail.com>
2025-05-14 22:02:14 +0200

    Modify Step1

--- eyra-backend/src/Controller/AuthController.php
+++ eyra-backend/src/Controller/AuthController.php
@@ -462,7 +462,7 @@ class AuthController extends AbstractController
         }
     }
 
-    #[Route('/onboarding', name: 'api_complete_onboarding', methods: ['POST'])]
+    #[Route('/sav-onboarding', name: 'api_complete_onboarding', methods: ['POST'])]
     public function completeOnboarding(Request $request): JsonResponse
     {
         try {
