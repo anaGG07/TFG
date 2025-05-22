8b1e8c9f4483ff0589155818b92d32860b65f94d
anaGG07 <ana24garcia07@gmail.com>
2025-05-22 18:33:33 +0200

    Update index.html

--- eyra-frontend/src/layouts/RootLayout.tsx
+++ eyra-frontend/src/layouts/RootLayout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import { CycleProvider } from "../context/CycleContext";
-import SmartRedirector from "../router/SmartRedirector";
 
 const RootLayout = () => {
   return (
     <AuthProvider>
       <CycleProvider>
-        <SmartRedirector />
         <div className="w-screen h-screen overflow-hidden bg-bg text-primary font-sans flex flex-col items-center justify-center relative" style={{ minHeight: '100vh', minWidth: '100vw' }}>
           <main className="flex-1 w-full h-full flex items-center justify-center z-10">
             <Outlet />
