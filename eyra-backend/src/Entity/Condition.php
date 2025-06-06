bbf5a567eafff10c98f0e75fa7fae7e55d11931c
anaGG07 <ana24garcia07@gmail.com>
2025-06-06 22:05:54 +0200

    Update ConditionController

--- eyra-backend/src/Entity/Condition.php
+++ eyra-backend/src/Entity/Condition.php
@@ -7,21 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ConditionRepository::class)]
 #[ORM\HasLifecycleCallbacks]
-#[ApiResource(
-    normalizationContext: ['groups' => ['condition:read']],
-    denormalizationContext: ['groups' => ['condition:write']],
-    operations: [
-        new Get(),
-        new GetCollection()
-    ]
-)]
 class Condition
 {
     #[ORM\Id]
