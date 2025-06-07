5113a5112a15cbeb62adea25025cab6a6108766d
anaGG07 <ana24garcia07@gmail.com>
2025-06-07 21:56:15 +0200

    Update tracking

--- eyra-frontend/src/pages/AdminPage.tsx
+++ eyra-frontend/src/pages/AdminPage.tsx
@@ -302,18 +302,18 @@ const AdminPage = () => {
                           transition={{ duration: 0.4, ease: "easeInOut" }}
                           className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full justify-center mt-4 mb-4"
                         >
-                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#f8b4b4]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem]">
-                            <UsersSummaryIcon className="w-20 h-20" />
+                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#f8b4b4]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem] p-2">
+                            <UsersSummaryIcon className="w-28 h-28" />
                             <h3 className="text-base font-semibold text-[#C62328] font-serif">Usuarios</h3>
                             <p className="text-3xl font-bold text-[#991b1b]">{stats?.totalUsers?.toLocaleString() || "0"}</p>
                           </NeomorphicCard>
-                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#a7f3d0]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem]">
-                            <ActiveSummaryIcon className="w-20 h-20" />
+                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#a7f3d0]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem] p-2">
+                            <ActiveSummaryIcon className="w-28 h-28" />
                             <h3 className="text-base font-semibold text-[#15803d] font-serif">Activos</h3>
                             <p className="text-3xl font-bold text-[#15803d]">{stats?.activeUsers?.toLocaleString() || "0"}</p>
                           </NeomorphicCard>
-                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#ddd6fe]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem]">
-                            <AdminSummaryIcon className="w-20 h-20" />
+                          <NeomorphicCard className="flex flex-col items-center justify-center gap-3 bg-[#ddd6fe]/30 w-36 h-36 min-w-[8rem] min-h-[8rem] max-w-[9rem] max-h-[9rem] p-2">
+                            <AdminSummaryIcon className="w-28 h-28" />
                             <h3 className="text-base font-semibold text-[#7c2d12] font-serif">Admins</h3>
                             <p className="text-3xl font-bold text-[#7c2d12]">{stats?.adminUsers?.toLocaleString() || "0"}</p>
                           </NeomorphicCard>
